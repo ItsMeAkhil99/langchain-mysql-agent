2368c2a0de789e2ade3306a7d81e648f209b071f
AKHIL KUKKADAPU <117564934+ItsMeAkhil99@users.noreply.github.com>
2025-05-12 15:20:44 +0530

    Main File

---
+++ main.py
@@ -0,0 +1,25 @@
+from db_tool import create_mysql_db_tool
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY not found in .env")
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+# You can change query string here
+response = create_mysql_db_tool(
+    username="root",
+    password="1234",
+    host="localhost",
+    port="3306",
+    database_name="test_agent",
+    model_name="gpt-3.5-turbo",
+    query="Tell me how many female drivers who are blonde"
+)
+
+print("🧠 Response:\n", response)
